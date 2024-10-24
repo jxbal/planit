25dd044107813ff1a63c0cf58483f606139f54c3
Owen Dewing <odewing@lion.lmu.edu>
2024-10-23 19:04:42 -0700

    setting up main page initially

---
+++ musicTaste/app/(tabs)/main.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Image, Platform } from 'react-native';
+import { View, Text } from 'react-native';
+
+export default function MainPage() {
+    return(
+        <View style={styles.mainPage}>
+            <Text testID="mainPagetitle">Hi @username</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    mainPage: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+  });
